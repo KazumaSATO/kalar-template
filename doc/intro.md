7967cdeabdd9133ac160c2222a99d8cbc7f64b3f
Kazuma SATO <ranceworks@gmail.com>
2015-11-23 21:04:19 +0900

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kalar-demo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kalar_demo/core_test.clj
@@ -0,0 +1,7 @@
+(ns kalar-demo.core-test
+  (:require [clojure.test :refer :all]
+            [kalar-demo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
