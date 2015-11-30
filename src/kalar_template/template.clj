c8440e25a66489d3fc0558f2e2f120e1484e03f8
Kazuma SATO <ranceworks@gmail.com>
2015-11-30 20:24:50 +0900

    #4 bootstrap

--- src/kalar_template/template.clj
+++ src/kalar_template/template.clj
@@ -1,5 +1,28 @@
 (ns kalar-template.template
-  (:require [kalar-plugins.templates.hiccup :as hp]))
+  (:require [kalar-plugins.templates.hiccup :as hp]
+            [hiccup.page :as hpage]))
 
-(hp/def-template "index.html" "sadabar")
-
+(hp/def-template
+  "index.html"
+  (hpage/html5
+    [:head
+     [:title "Home | Compojure Docs"]
+     (hpage/include-js "/js/jquery-2.1.4.min.js")
+     (hpage/include-js "/js/bootstrap.min.js")
+     (hpage/include-css "/css/bootstrap.min.css")]
+    [:body
+     [:nav {:class "navbar navbar-default navbar-fixed-top"}
+      [:div {:class "container"}
+       [:div {:class "navbar-header"}
+        [:button {:type "button"
+                  :class "navbar-toggle collapsed"
+                  :data-toggle "collapse"
+                  :data-target "#navbar"
+                  :aria-expanded "false"
+                  :aria-controls "navbar"}
+         (concat '([:span {:class "sr-only"} "Toggle navigation"])
+                 (repeat 3 [:span {:class "icon-bar"}]))]
+        [:a {:class "navbar-brand" :href "#"} "Project Name"]]
+       [:div {:id "navbar" :class "navbar-collapse collapse"}
+        [:ul {:class "nav navbar-nav"}
+         (for [e '("Action" "About")] [:li [:a {:href "#"} e]])]]]]]))
