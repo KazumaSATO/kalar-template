a2c94f3bcf8f4a8b0d0fdd18535d556fdca2d096
Kazuma SATO <ranceworks@gmail.com>
2015-12-07 07:14:21 +0900

    #4 decorate single post page imperfectly

--- src/kalar_template/template.clj
+++ src/kalar_template/template.clj
@@ -36,15 +36,69 @@
        ]]
      ]))
 
-
-(hp/def-templates
-  "posts/:id/index.html"
-  "resources/posts"
-  (hpage/html5
-    [:head]
-    [:body "hoge"]))
-
 (def ^{:private true} config (kconfig/read-config))
+
+
+(defn- wrap-postframe [page inner]
+  (hpage/html5
+    [:head
+     [:title (:title config)]
+     (hpage/include-js "/js/jquery-2.1.4.min.js")
+     (hpage/include-js "/js/bootstrap.min.js")
+     (hpage/include-css "/css/bootstrap.min.css")
+     (hpage/include-css "/css/kalar.css")]
+    [:body
+     [:nav {:class "navbar navbar-default navbar-fixed-top"}
+      [:div {:class "container"}
+       [:div {:class "navbar-header"}
+        [:button {:type "button"
+                  :class "navbar-toggle collapsed"
+                  :data-toggle "collapse"
+                  :data-target "#navbar"
+                  :aria-expanded "false"
+                  :aria-controls "navbar"}
+         (concat '([:span {:class "sr-only"} "Toggle navigation"])
+                 (repeat 3 [:span {:class "icon-bar"}]))]
+        [:a {:class "navbar-brand" :href "#"} "Project Name"]]
+       [:div {:id "navbar" :class "navbar-collapse collapse"}
+        [:ul {:class "nav navbar-nav"}
+         (for [e '("Action" "About")] [:li [:a {:href "#"} e]])]]]]
+     [:div {:class "container"}
+      [:div {:class "site-header"} [:h1 {:class "text-center"} (:title config)]]
+      [:div {:class "row"}
+       [:div {:class "col-xs-12 col-sm-6 col-md-8"}
+        inner
+        [:nav [:ul {:class "pager"}
+               (if (-> page :previous-page nil?)
+                 [:li {:class "disabled"} [:a {:href "#"} "Previous"]]
+                 [:li [:a {:href (-> page :previous-page)} "Previous"]])
+               (if (-> page :next-page nil?)
+                 [:li {:class "disabled"} [:a {:href "#"} "Next"]]
+                 [:li [:a {:href (-> page :next-page)} "Next"]])]]]
+       [:div {:class "col-xs-6 col-md-4"} "hoge"]]]]))
+
+(hp/def-posts
+  (wrap-postframe page_
+    [:article {:class "post"}
+     [:header
+      [:div {:class "row"}
+       [:div {:class "col-xs-12 col-sm-9"}
+        [:h2 (-> page_ :title first)]]
+       [:div {:class "col-sm-3 post-date"}
+        [:time {:class "block"} "NOVENVER 15, 2015"]
+        [:span "category"]]]]
+     (:body page_)
+     [:footer
+      [:div {:class "row"}
+       [:div {:class "col-sm-12 col-md-4 col-ld-3"}
+        [:a {:type "button"
+             :class "btn btn-default btn-lg btn-block"
+             :role "button"
+             :href (:post-url page_)}
+         "Continue Reading"]]
+       [:div {:class "col-md-8 col-ld-9"}]
+       ]]]))
+
 
 (hp/def-excerpts
   "index.html" "page:num.html" 3
