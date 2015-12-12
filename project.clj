a47db98ee1c340b254151cc8437da17251eb6fa8
Kazuma SATO <ranceworks@gmail.com>
2015-12-12 12:59:21 +0900

    #2 change license to MIT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject kalar-template "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/KazumaSATO/kalar-template"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [hiccup "1.0.5"]
                  [clj-yaml "0.4.0"]
