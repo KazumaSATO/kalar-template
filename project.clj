664820b6bdad528288098a89cf99157d66032dff
Kazuma SATO <ranceworks@gmail.com>
2015-12-16 06:56:40 +0900

    v0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kalar-template "0.1.3"
+(defproject kalar-template "0.1.5"
   :description "a demo for kalar"
   :url "https://github.com/KazumaSATO/kalar-template"
   :license {:name "The MIT License"
@@ -7,7 +7,7 @@
                  [hiccup "1.0.5"]
                  [clj-yaml "0.4.0"]
                  [compojure "1.4.0"]
-                 [kalar-plugins "0.1.3"]]
+                 [kalar-plugins "0.1.5"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler kalar-core.server/app
          :init kalar-core.server/init
