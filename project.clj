e89e7a88ca5eda61a2344fcbcda59bdafce00016
Kazuma SATO <ranceworks@gmail.com>
2015-12-19 00:24:10 +0900

    update plugin version

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [hiccup "1.0.5"]
                  [clj-yaml "0.4.0"]
                  [compojure "1.4.0"]
-                 [kalar-plugins "0.1.5"]]
+                 [kalar-plugins "0.1.6"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler kalar-core.server/app
          :init kalar-core.server/init
